fd77b1c212cb034f5a38d592beee043b65e28b36
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2022-12-14 11:17:36 +0100

    Developing the code.

--- src/PROG/P_5/P_5_2/P_5_2_2_maquinaVending.java
+++ src/PROG/P_5/P_5_2/P_5_2_2_maquinaVending.java
@@ -2,24 +2,33 @@ package PROG.P_5.P_5_2;
 
 import java.util.Scanner;
 
+import static ProgramasPruebas.metodos.menuVending;
+import static ProgramasPruebas.metodos.printOptions;
+
 public class P_5_2_2_maquinaVending {
-//    Daniel Alonso Lázaro - 2022
-
-//    Al comienzo del día, la máquina dispone de 20 monedas de todas las cantidades
-//    necesarias para el cambio.
-//    Se debe dar el cambio correcto, con el menor número de monedas posibles. Quiero
-//    que aparezca en pantalla un mensaje de “INTRODUZCA IMPORTE EXACTO” si
-//    la máquina no dispone de monedas en 2 tipos de estas o más o si no dispone de
-//    monedas de un solo tipo y este es el de 5 cts., y que solo acepte este dinero.
-
+    //    Daniel Alonso Lázaro - 2022
+    /*
+    Disponemos de una máquina automática de venta de bebidas. Disponemos de tres productos:
+    agua cuyo precio es 0,50, refresco con precio de 0,75 y zumo cuyo precio es de 0,95 cts.
+    El programa emitirá un menú que mostrará los productos y sus precios, así como la opción de salir.
+    El programa pedirá la opción elegida y el dinero que se introduce para pagar.
+    La forma de introducir el dinero para pagar en el programa es introducir el valor de cada moneda que ponemos
+    en la máquina expresada en céntimos. El programa aceptará monedas hasta que se llegue al importe solicitado o se
+    sobrepase la primera vez.
+    Nuestra máquina acepta todas las monedas de euro a excepción de los 2 cts. y 1 cts.
+    Al comienzo del día, la máquina dispone de 20 monedas de todas las cantidades necesarias para el cambio.
+    Se debe dar el cambio correcto, con el menor número de monedas posibles. Quiero que aparezca en pantalla un
+    mensaje de “INTRODUZCA IMPORTE EXACTO” si la máquina no dispone de monedas en 2 tipos de estas o más o si no
+    dispone de monedas de un solo tipo y este es el de 5 cts., y que solo acepte este dinero.
+    Al finalizar el programa nos debe dar el total del dinero disponible en la máquina, por unidad monetaria
+    */
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int precio, dineroCents = 0;
-        double cambio;
+        int precio, dineroCents, cambio;
         String producto;
-        int[] monedas = {5, 10, 20, 50, 100, 200};
-        int[] cantidad = {20, 20, 20, 20, 20, 20};
+        int[] monedas = {200, 100, 50, 20, 10, 5};
+        int[] cantidad = {20, 5, 5, 20, 20, 1};
         do {
             do {
                 System.out.print("""
@@ -46,47 +55,49 @@ public class P_5_2_2_maquinaVending {
                         precio = 0;
                     }
                 }
-            } while (!producto.equalsIgnoreCase("agua") &&
+            }while(!producto.equalsIgnoreCase("agua") &&
                     !producto.equalsIgnoreCase("refresco") &&
                     !producto.equalsIgnoreCase("zumo"));
-            do {
-                System.out.print("""
-                        
-                        -2€
-                        -1€
-                        -50cts
-                        -20cts
-                        -10cts
-                        -5cts
-                        -2cts
-                        -1ct
-                        Introducir moneda:\040""");
-                switch (sc.nextLine().trim().toLowerCase()) {
-                    case "2" -> dineroCents += 200;
-                    case "1" -> dineroCents += 100;
-                    case "50" -> dineroCents += 50;
-                    case "20" -> dineroCents += 20;
-                    case "10" -> dineroCents += 10;
-                    case "5" -> dineroCents += 5;
-                    default -> System.out.println("\nMoneda no aceptada");
-                }
-                if (dineroCents < precio)
-                    System.out.printf("Dinero insuficiente, faltan %.2f euros\n", ((precio - dineroCents)/100.0));
-            } while (dineroCents < precio);
 
-            cambio = (dineroCents - precio) / 100.0;
+            int contadorCambio = 0;
 
-            if (cambio > 0) {
-                System.out.printf("Su cambio es de %2.2f euros.\n", cambio);
+            if(cantidad[5] < 1) {
+                contadorCambio = 3;
+            }else if (cantidad[0] < 1 || cantidad[1] < 1 || cantidad[2] < 1 || cantidad[3] < 1 || cantidad[4] < 1) {
+                contadorCambio = 3;
             }
-            System.out.print("Introduzca otro producto o 'salir' para terminar: ");
+
+            dineroCents = menuVending(precio, contadorCambio);
+
+            cambio = (dineroCents - precio);
+
+            System.out.println("\nExpendiendo producto y cambio");
+            System.out.println("\tProducto: "+producto.toUpperCase()+".");
+            System.out.println("\tCambio: "+cambio/100.0+"€");
+            for(int i = 0; i < monedas.length; i++){
+                if(monedas[i] > 99){
+                    if(cambio >= monedas[i]){
+                        System.out.println("\t\tMonedas de "
+                                +((monedas[i]/100.0)%1==0.0
+                                ? (int)(monedas[i]/100.0)
+                                : String.format("%.2f", (monedas[i]/100.0)))
+                                +" euro: "+cambio/monedas[i]);
+                        cambio%=monedas[i];
+                        cantidad[i]-=1;
+                    }
+                }else{
+                    if(cambio >= monedas[i]){
+                        System.out.println("\t\tMonedas de "+(monedas[i]/100.0)+" céntimos: "+cambio/monedas[i]);
+                        cambio %= monedas[i];
+                        cantidad[i]-=1;
+                    }
+                }
+            }
+
+            System.out.print("\nIntroduzca 'salir' para terminar o pulse 'enter' para continuar: ");
             producto = sc.nextLine().trim().toLowerCase();
+            System.out.println();
         }while(!producto.equalsIgnoreCase("salir"));
     }
-
-    public static void printOptions(int precio, String producto) {
-        System.out.printf("El precio del %s es %.2f€\n", producto, precio/100.0);
-    }
-
 }
 
