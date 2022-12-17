aa2cc916799555854452829c681c25a2e34e6072
GyllenhaalSP <dalonsolaz@gmail.com>
2022-12-17 22:56:23 +0100

    Simplified version with methods extracted. Structure changed.

--- src/PROG/P_5/P_5_2/P_5_2_2_maquinaVending.java
+++ src/PROG/P_5/P_5_2/P_5_2_2_maquinaVending.java
@@ -1,9 +1,8 @@
 package PROG.P_5.P_5_2;
 
+import java.util.Arrays;
 import java.util.Scanner;
-
-import static ProgramasPruebas.metodos.menuVending;
-import static ProgramasPruebas.metodos.printOptions;
+import static ProgramasPruebas.metodos.*;
 
 public class P_5_2_2_maquinaVending {
     //    Daniel Alonso Lázaro - 2022
@@ -25,79 +24,29 @@ public class P_5_2_2_maquinaVending {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int precio, dineroCents, cambio;
+        int precio, cambio;
         String producto;
         int[] monedas = {200, 100, 50, 20, 10, 5};
-        int[] cantidad = {20, 5, 5, 20, 20, 1};
+        int[] cantidad = {20, 1, 1, 1, 1, 1};
         do {
-            do {
-                System.out.print("""
-                        -Agua
-                        -Refresco
-                        -Zumo
-                        -Salir
-                        Elegir un producto:\040""");
-                switch (producto = sc.nextLine().trim().toLowerCase()) {
-                    case "agua" -> {
-                        precio = 50;
-                        printOptions(precio, producto);
-                    }
-                    case "refresco" -> {
-                        precio = 75;
-                        printOptions(precio, producto);
-                    }
-                    case "zumo" -> {
-                        precio = 95;
-                        printOptions(precio, producto);
-                    }
-                    default -> {
-                        System.out.println("\nProducto no disponible");
-                        precio = 0;
-                    }
-                }
-            }while(!producto.equalsIgnoreCase("agua") &&
-                    !producto.equalsIgnoreCase("refresco") &&
-                    !producto.equalsIgnoreCase("zumo"));
-
-            int contadorCambio = 0;
-
-            if(cantidad[5] < 1) {
-                contadorCambio = 3;
-            }else if (cantidad[0] < 1 || cantidad[1] < 1 || cantidad[2] < 1 || cantidad[3] < 1 || cantidad[4] < 1) {
-                contadorCambio = 3;
+            String[] menu = menuPrincipal();
+            producto = menu[0];
+            precio = Integer.parseInt(menu[1]);
+            System.out.println(Arrays.toString(cantidad));
+            if(!producto.equalsIgnoreCase("salir")) {
+                cambio = (menuVending(precio, cantidad, monedas) - precio);
+                System.out.println("\nExpendiendo producto y cambio");
+                System.out.println("\tProducto: " + producto.toUpperCase() + ".");
+                System.out.println("\tCambio: " + cambio / 100.0 + "€");
+                cambio(cambio, monedas, cantidad);
+                System.out.println(Arrays.toString(cantidad));
+                System.out.print("\nIntroduzca 'salir' para terminar o pulse 'enter' para continuar: ");
+                producto = sc.nextLine().trim().toLowerCase();
+                System.out.println();
+                System.out.println(Arrays.toString(cantidad));
             }
-
-            dineroCents = menuVending(precio, contadorCambio);
-
-            cambio = (dineroCents - precio);
-
-            System.out.println("\nExpendiendo producto y cambio");
-            System.out.println("\tProducto: "+producto.toUpperCase()+".");
-            System.out.println("\tCambio: "+cambio/100.0+"€");
-            for(int i = 0; i < monedas.length; i++){
-                if(monedas[i] > 99){
-                    if(cambio >= monedas[i]){
-                        System.out.println("\t\tMonedas de "
-                                +((monedas[i]/100.0)%1==0.0
-                                ? (int)(monedas[i]/100.0)
-                                : String.format("%.2f", (monedas[i]/100.0)))
-                                +" euro: "+cambio/monedas[i]);
-                        cambio%=monedas[i];
-                        cantidad[i]-=1;
-                    }
-                }else{
-                    if(cambio >= monedas[i]){
-                        System.out.println("\t\tMonedas de "+(monedas[i]/100.0)+" céntimos: "+cambio/monedas[i]);
-                        cambio %= monedas[i];
-                        cantidad[i]-=1;
-                    }
-                }
-            }
-
-            System.out.print("\nIntroduzca 'salir' para terminar o pulse 'enter' para continuar: ");
-            producto = sc.nextLine().trim().toLowerCase();
-            System.out.println();
         }while(!producto.equalsIgnoreCase("salir"));
+
+        cierreMaquina(cantidad, monedas);
     }
 }
-
