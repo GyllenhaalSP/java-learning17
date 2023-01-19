a9533cc0140e3b38c24f2429b4a7953e703f2ce8
Maki <110998002+spariva@users.noreply.github.com>
2023-01-19 03:31:35 +0100

    Update Main.java
    Esto es una prueba

--- src/EV2/OOP/C_230112/Main.java
+++ src/EV2/OOP/C_230112/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Vehiculo vehiculo1 = new Vehiculo();
 
         vehiculo1.setMarca("Ford");
-        vehiculo1.setModelo("Fiesta");
+        vehiculo1.setModelo("Fiestuki");
         vehiculo1.setNumPasajeros(5);
         vehiculo1.setCapacidadCombustible(50);
         vehiculo1.setConsumo(6.3);
