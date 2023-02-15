f5dc59d3e71463a663cece0ff48dad082f135354
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-02-15 17:57:23 +0100

    Removed magic numbers.

--- src/EV2/OOP/EjDivEntYMod/EjDivEntYMod2.java
+++ src/EV2/OOP/EjDivEntYMod/EjDivEntYMod2.java
@@ -5,24 +5,24 @@ import static ProgramasPruebas.Metodos.mostrarArray;
 
 public class EjDivEntYMod2 {
     public static void main(String[] args) {
+        final int FILAS = 4;
+        final int COLUMNAS = 7;
+        final int MAX_VALOR_RANDOM = 100;
         Random rand = new Random();
-        int[][] matriz = new int[4][7];
+        int[][] matriz = new int[FILAS][COLUMNAS];
 
-        for (int i = 0; i < 4*7; i++) {
-            matriz[i%4][i/4] = rand.nextInt(100);
+        for (int i = 0; i < FILAS*COLUMNAS; i++) {
+            matriz[i%FILAS][i/FILAS] = rand.nextInt(MAX_VALOR_RANDOM);
         }
 
-        mostrarArray("El array es: ", matriz, 7);
+        mostrarArray("El array es: ", matriz, COLUMNAS);
 
-        int[][] transMatriz = new int[7][4];
+        int[][] transMatriz = new int[COLUMNAS][FILAS];
 
-        for (int i = 0; i < 4*7; i++){
-            transMatriz[i/4][i%4] = matriz[i%4][i/4];
+        for (int i = 0; i < FILAS*COLUMNAS; i++){
+            transMatriz[i/FILAS][i%FILAS] = matriz[i%FILAS][i/FILAS];
         }
 
-        mostrarArray("El array traspuesto es: ", transMatriz, 4);
-
-
-
+        mostrarArray("El array traspuesto es: ", transMatriz, FILAS);
     }
 }
