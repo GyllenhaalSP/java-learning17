43bc215136413af0846c011117176570efc02d59
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-03-08 00:44:32 +0100

    Ready to deliver. Added javadoc and compiling instructions.

--- src/EV2/OOP/C_20230309/MainFibonacci.java
+++ src/EV2/OOP/C_20230309/MainFibonacci.java
@@ -1,5 +1,12 @@
 package EV2.OOP.C_20230309;
 
+//Compilar con 'javac -encoding utf8 MainFibonacci.java' para mostrar acentos.
+
+/**
+ * Clase contenedora del método recursivo para obtener el número de fibonacci de n iteraciones.
+ * @author Daniel Alonso Lázaro - 2023
+ * @version 1.0
+ */
 public class MainFibonacci {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
