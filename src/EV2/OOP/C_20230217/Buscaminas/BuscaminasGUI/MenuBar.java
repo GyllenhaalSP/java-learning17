3ced483eb8edccc68fa728867c12cb59e0b1bc77
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-03-26 15:02:08 +0200

    Moved and renamed things around. Reformatted file. Added javadocs. Final version.

--- src/EV2/OOP/C_20230217/Buscaminas/BuscaminasGUI/MenuBar.java
+++ src/EV2/OOP/C_20230217/Buscaminas/BuscaminasGUI/MenuBar.java
@@ -6,16 +6,20 @@ import javax.swing.*;
  * Clase MenuBar.
  * Extiende JMenuBar y crea la barra general de los menús del juego en todas las instancias
  * del frame principal.
+ *
  * @author Daniel Alonso Lázaro - 2023
  * @version 2.0
  */
-public class MenuBar extends JMenuBar{
+public class MenuBar extends JMenuBar {
     JMenu archivo;
     JMenu opciones;
     JMenu help;
     JMenu setDificultad;
+    JMenu personalizado;
+    JMenuItem nuevoJuego;
+    JMenuItem reiniciar;
     JMenuItem salir;
-    JMenuItem about;
+    JMenuItem sobreMi;
     JMenuItem setFilasYColumnas;
     JMenuItem setMinas;
     JMenuItem facil;
@@ -23,35 +27,46 @@ public class MenuBar extends JMenuBar{
     JMenuItem dificil;
     JMenuItem pro;
 
-
-    public MenuBar(JFrame frame){
+    // Constructor
+    public MenuBar(JFrame frame) {
         archivo = new JMenu("Archivo");
+        nuevoJuego = new JMenuItem("Nuevo juego");
+        nuevoJuego.addActionListener(e -> {
+            frame.dispose();
+            new Bienvenida(GUI.getWidth(), GUI.getHeight());
+        });
+        reiniciar = new JMenuItem("Reiniciar");
+        reiniciar.addActionListener(e -> {
+            frame.dispose();
+            new GUI(CuadriculaJuego.seleccionModoJuego(frame));
+        });
         salir = new JMenuItem("Salir");
         salir.addActionListener(e -> System.exit(0));
+        archivo.add(nuevoJuego);
+        archivo.add(reiniciar);
         archivo.add(salir);
         this.add(archivo);
 
         opciones = new JMenu("Opciones");
 
+        personalizado = new JMenu("Personalizado");
         setFilasYColumnas = new JMenuItem("Filas y columnas");
         setFilasYColumnas.addActionListener(e -> {
-            while(true) {
-                String filas = introduccionDatos(frame, "Introduce el número de filas.\n" +
-                        "Este número será también el número de columnas.", "Filas y Columnas");
+            while (true) {
+                String filas = introduccionDatos(frame, "Introduce el número de filas.\n" + "Este número será también el número de columnas.", "Filas y Columnas");
                 if (filas != null) {
                     try {
                         int filasInt = Integer.parseInt(filas);
-                        if (filasInt > 0 && filasInt <= 30){
+                        if (filasInt > 0 && filasInt <= 31) {
                             CuadriculaJuego.setDimensionUsuario(filasInt);
                             break;
                         } else {
-                            errorValidacion(frame, "El número introducido debe ser mayor que 0");
+                            errorValidacion(frame, "El número introducido debe ser mayor que 0 y menor que 30");
                         }
                     } catch (NumberFormatException ex) {
-                        errorValidacion(frame, "El número introducido no es un número " +
-                                "o debe ser un número entero");
+                        errorValidacion(frame, "El número introducido no es un número " + "o debe ser un número entero");
                     }
-                } else{
+                } else {
                     break;
                 }
             }
@@ -59,57 +74,46 @@ public class MenuBar extends JMenuBar{
 
         setMinas = new JMenuItem("Minas");
         setMinas.addActionListener(e -> {
-            while(true) {
+            while (true) {
                 String minas = introduccionDatos(frame, "Introduce el número de minas", "Minas");
                 if (minas != null) {
                     try {
                         int minasInt = Integer.parseInt(minas);
-                        if (minasInt > 0) {
-                            CuadriculaJuego.setNumeroMinasUsuario(minasInt);
-                            break;
+                        double minasMax = Math.ceil(CuadriculaJuego.getDimensionUsuario() * 1.15f);
+                        if (CuadriculaJuego.getDimensionUsuario() > 0) {
+                            if (minasInt > 0 && minasInt < minasMax) {
+                                CuadriculaJuego.setNumeroMinasUsuario(minasInt);
+                                break;
+                            } else {
+                                errorValidacion(frame, "El número introducido debe ser mayor que 0 y menor que " + (int) minasMax);
+                            }
                         } else {
-                            errorValidacion(frame, "El número introducido debe ser mayor que 0");
+                            errorValidacion(frame, "Debes introducir primero el número de filas y columnas.");
+                            break;
                         }
                     } catch (NumberFormatException ex) {
-                        errorValidacion(frame, "El número introducido no es un número " +
-                                "o debe ser un número entero");
+                        errorValidacion(frame, "El número introducido no es un número " + "o debe ser un número entero");
                     }
-                }else{
+                } else {
                     break;
                 }
             }
         });
-        opciones.add(setFilasYColumnas);
-        opciones.add(setMinas);
+
+        personalizado.add(setFilasYColumnas);
+        personalizado.add(setMinas);
+        opciones.add(personalizado);
+
         setDificultad = new JMenu("Dificultad");
+
         facil = new JMenuItem("Fácil");
-        facil.addActionListener(e -> {
-            CuadriculaJuego.setFacil(true);
-            CuadriculaJuego.setMedio(false);
-            CuadriculaJuego.setDificil(false);
-            CuadriculaJuego.setPro(false);
-        });
+        facil.addActionListener(e -> CuadriculaJuego.setDificultad(true, false, false, false));
         medio = new JMenuItem("Medio");
-        medio.addActionListener(e -> {
-            CuadriculaJuego.setFacil(false);
-            CuadriculaJuego.setMedio(true);
-            CuadriculaJuego.setDificil(false);
-            CuadriculaJuego.setPro(false);
-        });
+        medio.addActionListener(e -> CuadriculaJuego.setDificultad(false, true, false, false));
         dificil = new JMenuItem("Difícil");
-        dificil.addActionListener(e -> {
-            CuadriculaJuego.setFacil(false);
-            CuadriculaJuego.setMedio(false);
-            CuadriculaJuego.setDificil(true);
-            CuadriculaJuego.setPro(false);
-        });
+        dificil.addActionListener(e -> CuadriculaJuego.setDificultad(false, false, true, false));
         pro = new JMenuItem("Pro");
-        pro.addActionListener(e -> {
-            CuadriculaJuego.setFacil(false);
-            CuadriculaJuego.setMedio(false);
-            CuadriculaJuego.setDificil(false);
-            CuadriculaJuego.setPro(true);
-        });
+        pro.addActionListener(e -> CuadriculaJuego.setDificultad(false, false, false, true));
         setDificultad.add(facil);
         setDificultad.add(medio);
         setDificultad.add(dificil);
@@ -118,21 +122,37 @@ public class MenuBar extends JMenuBar{
         this.add(opciones);
 
         help = new JMenu("?");
-        about = new JMenuItem("Sobre mi");
-        about.addActionListener(e -> JOptionPane.showMessageDialog(frame, """
+        sobreMi = new JMenuItem("Sobre mi");
+        sobreMi.addActionListener(e -> JOptionPane.showMessageDialog(frame, """
                 Buscaminas 2.0
                 Creado por: Daniel Alonso Lázaro
                 IES Juan de la Cierva - DAW1V 2022/2023
-                Primera revisión: 15/03/2023""", "Sobre mi", JOptionPane.INFORMATION_MESSAGE));
-        help.add(about);
+                Primera revisión: 15/03/2023
+                Última revisión: 26/03/2023""", "Sobre mi", JOptionPane.INFORMATION_MESSAGE));
+        help.add(sobreMi);
         this.add(help);
     }
 
+    /**
+     * Muestra un mensaje de error en un JOptionPane.
+     *
+     * @param frame   frame en el que se mostrará el mensaje.
+     * @param mensaje mensaje que se mostrará.
+     */
     private void errorValidacion(JFrame frame, String mensaje) {
         JOptionPane.showMessageDialog(frame, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
-    private String introduccionDatos(JFrame frame, String mensaje, String titulo){
+    /**
+     * Muestra un JOptionPane para introducir datos.
+     *
+     * @param frame   frame en el que se mostrará el JOptionPane.
+     * @param mensaje mensaje que se mostrará.
+     * @param titulo  título del JOptionPane.
+     * @return String con el texto introducido.
+     */
+    private String introduccionDatos(JFrame frame, String mensaje, String titulo) {
         return JOptionPane.showInputDialog(frame, mensaje, titulo, JOptionPane.QUESTION_MESSAGE);
     }
+
 }
