75bc3c6cb7d71f5b2af4100a49ff4f869c0bc410
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-03-26 15:02:40 +0200

    Renamed class. Final version.

--- src/EV2/OOP/C_20230217/Buscaminas/BuscaminasGUI/Main.java
+++ src/EV2/OOP/C_20230217/Buscaminas/BuscaminasGUI/Main.java
@@ -2,11 +2,12 @@ package EV2.OOP.C_20230217.Buscaminas.BuscaminasGUI;
 
 /**
  * Clase principal del juego Buscaminas.
+ *
  * @author Daniel Alonso Lázaro - 2023
  * @version 2.0
  */
-public class MainSwing {
+public class Main {
     public static void main(String[] args) {
-        new Bienvenida();
+        new Bienvenida(GUI.getWidth(), GUI.getHeight());
     }
 }
