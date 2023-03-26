080bfe499b54fa4bb4317377313d9b42157f5f11
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-03-26 14:50:55 +0200

    Code cleanup. Magic numbers removal. Text formatting. Final version.

--- src/EV2/OOP/C_20230217/Buscaminas/BuscaminasGUI/Ayuda.java
+++ src/EV2/OOP/C_20230217/Buscaminas/BuscaminasGUI/Ayuda.java
@@ -9,19 +9,23 @@ import java.awt.*;
 /**
  * Clase Ayuda.
  * Clase que crea la interfaz de ayuda del juego.
+ *
  * @author Daniel Alonso Lázaro - 2023
  * @version 2.0
  */
 public class Ayuda extends JFrame {
+    // Atributos
     JFrame frame;
     JLabel helpHeader;
     JTextPane helpText;
     JButton botonVolver;
     JScrollPane scrollPane;
-    public Ayuda() {
+
+    // Constructor
+    public Ayuda(int width, int height) {
         frame = new JFrame("AYUDA");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
+        frame.setSize(width, height);
         frame.setResizable(false);
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
@@ -29,11 +33,11 @@ public class Ayuda extends JFrame {
 
         helpHeader = new JLabel("MENÚ DE AYUDA");
         helpHeader.setHorizontalAlignment(JLabel.CENTER);
-        helpHeader.setFont(new Font("Lucida Console", Font.BOLD, 40));
+        helpHeader.setFont(new Font("Lucida Console", Font.BOLD, GUI.TITLE_FONT_SIZE));
         helpHeader.setOpaque(true);
         helpHeader.setBackground(Color.BLACK);
         helpHeader.setForeground(Color.GREEN);
-        helpHeader.setPreferredSize(new Dimension(570, 60));
+        helpHeader.setPreferredSize(new Dimension(width, (int) (height * GUI.GUI_RESIZING_VALUE)));
         frame.add(helpHeader, BorderLayout.NORTH);
 
         helpText = new JTextPane();
@@ -41,17 +45,19 @@ public class Ayuda extends JFrame {
         SimpleAttributeSet align = new SimpleAttributeSet();
         StyleConstants.setAlignment(align, StyleConstants.ALIGN_JUSTIFIED);
         style.setParagraphAttributes(0, style.getLength(), align, false);
-        helpText.setFont(new Font("Lucida Console", Font.BOLD, 20));
+        helpText.setFont(new Font("Lucida Console", Font.BOLD, GUI.HELP_TEXT_FONT_SIZE));
         helpText.setOpaque(true);
         helpText.setBackground(Color.BLACK);
         helpText.setForeground(Color.GREEN);
         helpText.setText("""
                 El objetivo del buscaminas es descubrir todo el área de juego.
-                
+                                
                 Para ello, se deben descubrir todas las casillas que NO contienen minas.
 
-                Al introducir la coordenada de una casilla, se descubrirá su contenido. Si la celda está vacía, se descubrirán las casillas adyacentes. El número que se revela en estas celdas adyacentes indica las minas que hay en las ocho casillas inmediatamente adyacentes a la celda descubierta.
-                
+                Al introducir la coordenada de una casilla, se descubrirá su contenido.
+                                
+                Si la celda está vacía, se descubrirán las casillas adyacentes. El número que se revela en estas celdas adyacentes indica las minas que hay en las ocho casillas inmediatamente adyacentes a la celda descubierta.
+                                
                 Si se descubre una casilla con una mina, se pierde automáticamente la partida.
 
                 Si se está seguro de que una casilla contiene una mina, se puede marcar con una bandera. Para marcar una celda con una bandera, se tiene que introducir su coordenada. No se puede poner una bandera en una celda que ya esté descubierta ni en una casilla que ya tenga una bandera.
@@ -60,7 +66,7 @@ public class Ayuda extends JFrame {
 
                 ¡Buena suerte!
                 """);
-        helpText.setMargin(new Insets(10, 10, 10, 10));
+        helpText.setMargin(new Insets(15, 30, 15, 15));
         helpText.setEditable(false);
         helpText.setCaretPosition(0);
 
@@ -68,21 +74,21 @@ public class Ayuda extends JFrame {
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         frame.add(scrollPane, BorderLayout.CENTER);
 
-        UIManager.put("Button.select", Color.BLACK );
+        UIManager.put("Button.select", Color.BLACK);
         botonVolver = new JButton("VOLVER");
         botonVolver.setFocusable(false);
         botonVolver.setOpaque(true);
         botonVolver.setBackground(Color.BLACK);
         botonVolver.setForeground(Color.GREEN);
-        botonVolver.setFont(new Font("Lucida Console", Font.BOLD, 30));
+        botonVolver.setFont(new Font("Lucida Console", Font.BOLD, GUI.GENERAL_FONT_SIZE));
         botonVolver.addActionListener(e -> {
-            new Bienvenida();
+            new Bienvenida(width, height);
             frame.dispose();
         });
-        botonVolver.setPreferredSize(new Dimension(570, 45));
+
+        botonVolver.setPreferredSize(new Dimension(width, (int) (height * GUI.GUI_RESIZING_VALUE)));
         botonVolver.setBorder(BorderFactory.createEmptyBorder());
         frame.add(botonVolver, BorderLayout.SOUTH);
+        frame.setJMenuBar(new MenuBar(frame));
     }
-
-
 }
